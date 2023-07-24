10f8cd4df3e0b5374168e930b2821ea25439c7eb
Victor Zhang <victor.zhang@digio.com.au>
2023-07-24 20:14:05 +1000

    clean up counter render

--- src/util/renderCounter.tsx
+++ src/util/renderCounter.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect, useRef } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { Typography } from '@mui/material';
 
 interface RenderCounterProps {
@@ -6,27 +6,16 @@ interface RenderCounterProps {
 }
 
 const RenderCounter: FC<RenderCounterProps> = ({componentName}) => {
-    // const [renderCount, setRenderCount] = useState(0);
+    const renderCount = useRef(1);
 
-    // useEffect(() => {
-    //     setRenderCount((prev) => prev + 1);
-    //     console.log('rendercount', renderCount)
-    // }, [componentName])
-    const renderCount = useRef(0);
-    renderCount.current = renderCount.current + 1;
-    console.log('renderCount', componentName, renderCount.current)
-    // useEffect(() => {
-    //     renderCount.current = renderCount.current + 1;
-    //     console.log('renderCount.current', renderCount.current)
-    // });
+    useEffect(() => {
+        renderCount.current = renderCount.current + 1;
+    });
 
     return (
         <Typography
             sx={{
                 border: '1px solid black',
-                // position: 'absolute',
-                // top: '0',
-                // right: '0',
             }}
         >
             {`${renderCount.current <= 1 ? '' : 're-'}render ${componentName}: x${renderCount.current}`}
